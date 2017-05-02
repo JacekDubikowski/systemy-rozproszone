f9f5dfaeee7bd918d068a7cf83283ec7fbc5a5ee
Jacek Dubikowski <jacek.dubikowski@gmail.com>
2017-05-02 19:58:30 +0200

    polishing part2

--- zad4/python/doctor.py
+++ zad4/python/doctor.py
@@ -6,19 +6,40 @@ import MedicalData_pb2_grpc
 
 def get_param(parameters):
     name = raw_input("Provide name of parameter: ")
-    minimum = float(raw_input("Provide min of parameter: "))
-    maximum = float(raw_input("Provide max of parameter: "))
+    minimum = get_float("Provide min of parameter: ")
+    maximum = get_float("Provide max of parameter: ")
     parameters.append(MedicalData_pb2.Parameter(name=name, min=minimum, max=maximum))
+
+
+def get_integer(string):
+    integer = raw_input(string)
+    while not integer.isdigit():
+        integer = raw_input("Try again. " + string)
+    return int(integer)
+
+
+def get_float(string):
+    def try_get_float(a):
+        try:
+            float(a)
+            return True
+        except ValueError:
+            return False
+    floating = raw_input(string)
+    while not try_get_float(floating):
+        floating = raw_input("Try again. " + string)
+    return float(floating)
+
 
 channel = grpc.insecure_channel('localhost:50051')
 stub = MedicalData_pb2_grpc.DoctorServiceStub(channel)
 
-id_patient = int(raw_input("Provide ID of patient (or 0 if you want to check all of them): "))
+id_patient = get_integer("Provide ID of patient (or 0 if you want to check all of them): ")
 not_finished = True
 params = []
 
 while not_finished:
-    decision = raw_input("Add parameter to check(\"add\")/finish adding and send query(\"fin\") ")
+    decision = raw_input("Add parameter to check(\"add\")/finish adding and send query(\"fin\"): ")
     if decision == "fin":
         not_finished = False
     elif decision == "add":
